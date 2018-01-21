65dc4fc50169570bd364ec699cfa2f95f5119c91
Ben Matare <ben.a.matare@gmail.com>
2018-01-21 17:11:08 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each array # put argument(s) here
     i = 0 #counter
      while i < array.length #while the counter is less then the length of the array
       i += 1 #contiually iterate
-      yield[i]
+      yield(array[i])
       end
       array
     end
