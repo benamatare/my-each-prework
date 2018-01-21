f8f0f5bcc7af3091a7ebe99c607502dc2183a28e
Ben Matare <ben.a.matare@gmail.com>
2018-01-21 17:20:28 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each array # put argument(s) here
+    i = 0 #counter
+     while i < array.length #while the counter is less then the length of the array
+      i += 1 #contiually iterate
+      yield(array[i])
+      end
+      array
+    end
+
+my_each(array) {|i| i+=1}
